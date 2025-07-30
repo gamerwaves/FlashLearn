ec8f29d2827a93bd319322d34e4752641b16121c
Dwait Pandhi <dwait.pandhi@gmail.com>
2025-07-30 19:07:00 -0400

    Initial Commit

---
+++ src/app.d.ts
@@ -0,0 +1,14 @@
+// /Users/dwait/Documents/Coding/Shipwrecked/FlashLearn/src/app.d.ts
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
+
+export {};
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<!-- /Users/dwait/Documents/Coding/Shipwrecked/FlashLearn/src/routes/+layout.svelte -->
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+{@render children()}
